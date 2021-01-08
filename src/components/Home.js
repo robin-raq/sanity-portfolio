1a22b3ff55bb709a0ce9739ecdc9e0155f8ec26c
Robinson <raqdrobinson@gmail.com>
2021-01-08 17:43:45 -0500

    style homepage

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import image from "../background-bear.jpg"
+
+export default function Home() {
+    return (
+        <main>
+            <img src={image} />
+            <section>
+                <h1> Hello World. I'm Raq!</h1>
+            </section>
+        </main>
+    )
+}
